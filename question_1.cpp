162071a0351139c90bdb74edc9d42248395df400
tungvuong2004 <vuongtritung@gmail.com>
2024-10-24 22:21:05 +0800

    Update question_1.cpp

--- question_1.cpp
+++ question_1.cpp
@@ -3,42 +3,32 @@
 
 using namespace std;
 
-// Function to insert an element at the bottom of a stack
 void insertAtBottom(stack<int>& s, int item) {
-    // Base case: If stack is empty, insert the item at the bottom
     if (s.empty()) {
         s.push(item);
         return;
     }
 
-    // Otherwise, pop the top element and recur
     int topElement = s.top();
     s.pop();
     insertAtBottom(s, item);
 
-    // Push the top element back after the recursive call
     s.push(topElement);
 }
 
-// Function to reverse the stack using recursion
 void reverseStack(stack<int>& s) {
-    // Base case: If stack is empty, return
     if (s.empty()) {
         return;
     }
 
-    // Pop the top element
     int topElement = s.top();
     s.pop();
 
-    // Recur for the rest of the stack
     reverseStack(s);
 
-    // Insert the popped element at the bottom
     insertAtBottom(s, topElement);
 }
 
-// Helper function to print the elements of the stack
 void printStack(stack<int> s) {
     while (!s.empty()) {
         cout << s.top() << " ";
@@ -50,7 +40,6 @@ void printStack(stack<int> s) {
 int main() {
     stack<int> s;
 
-    // Push elements into the stack
     s.push(1);
     s.push(2);
     s.push(3);
@@ -60,7 +49,6 @@ int main() {
     cout << "Original Stack: ";
     printStack(s);
 
-    // Reverse the stack
     reverseStack(s);
 
     cout << "Reversed Stack: ";
@@ -68,4 +56,5 @@ int main() {
 
     return 0;
 }
+
 
