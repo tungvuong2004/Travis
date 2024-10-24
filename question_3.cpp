5ef835461c6f0e9b9986798cb859d5e9725176bf
tungvuong2004 <vuongtritung@gmail.com>
2024-10-24 22:22:33 +0800

    Update question_3.cpp

--- question_3.cpp
+++ question_3.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+void printFirstNegativeInteger(vector<int>& arr, int k) {
+    deque<int> negatives;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        // Remove elements that are out of this window
+        if (!negatives.empty() && negatives.front() < i - k + 1) {
+            negatives.pop_front();
+        }
+
+        // If the current element is negative, add its index to the deque
+        if (arr[i] < 0) {
+            negatives.push_back(i);
+        }
+
+        // Print the first negative integer of the current window
+        if (i >= k - 1) {
+            if (!negatives.empty()) {
+                cout << arr[negatives.front()] << " ";
+            } else {
+                cout << "0 ";
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {-8, 2, 3, -6, 10};
+    int k = 2;
+
+    printFirstNegativeInteger(arr, k);
+
+    return 0;
+}
 
