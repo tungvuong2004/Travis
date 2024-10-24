4f4b46ce741fb129ed63b36e55b5b7432cee21a6
tungvuong2004 <vuongtritung@gmail.com>
2024-10-24 22:23:40 +0800

    Update question_4.cpp

--- question_4.cpp
+++ question_4.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+void interleaveQueue(queue<int>& q) {
+    int n = q.size();
+    if (n % 2 != 0) {
+        cout << "Queue must have an even length" << endl;
+        return;
+    }
+
+    queue<int> firstHalf;
+    
+    for (int i = 0; i < n / 2; i++) {
+        firstHalf.push(q.front());
+        q.pop();
+    }
+
+    while (!firstHalf.empty()) {
+        q.push(firstHalf.front());
+        firstHalf.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
+
+void printQueue(queue<int>& q) {
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    interleaveQueue(q);
+
+    printQueue(q);
+
+    return 0;
+}
 
