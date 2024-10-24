531617cc2b24492826b139cf10016952b38e5d03
tungvuong2004 <vuongtritung@gmail.com>
2024-10-24 22:21:55 +0800

    Update question_2.cpp

--- question_2.cpp
+++ question_2.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+void printNGE(vector<int>& arr) {
+    stack<int> s;
+    vector<int> nge(arr.size());
+
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        while (!s.empty() && s.top() <= arr[i]) {
+            s.pop();
+        }
+
+        if (s.empty()) {
+            nge[i] = -1;
+        } else {
+            nge[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " --> " << nge[i] << endl;
+    }
+}
+
+int main() {
+    vector<int> arr = {4, 5, 2, 25};
+    
+    printNGE(arr);
+
+    return 0;
+}
 
